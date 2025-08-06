30114b4b5cc130d7c323c9f4bb50dc2e7d118a0e
Michael Crowley <michaelcrowley931@gmail.com>
2025-08-06 19:26:53 +0000

    Disabled usage on mobile

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./homePage";
 import EditPage from "./editPage"; 
 import GamePage from "./gamePage";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 
 function Nbar(){
@@ -33,8 +33,43 @@ function Nbar(){
   }
 
 function App() {
-
   const [quoteList, setQuoteList] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize(); 
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div
+        style={{
+          paddingTop:"20vh",
+          textAlign: "center",
+          color:"white",
+          fontSize:"20px",
+          fontWeight:"bold",
+          margin:"auto"
+        }}>
+          <div style={{display:"block",margin:"auto",fontSize:"40px"}}>
+        <img src="chat.png" style={{height:"50px", width:"50px",display:"inline",marginBottom:"10px",marginRight:"10px"}}></img>LearnQuotes
+        </div>
+        <br />
+This app is only available on desktop devices. Please use a larger screen.
+      </div>
+    );
+  }
+
+
+
+
   return (
     <Router>
         <Nbar /> 
