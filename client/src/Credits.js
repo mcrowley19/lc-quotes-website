cec6e6828137c6de1a7d2f9e77b8d591080cc051
Michael Crowley <michaelcrowley931@gmail.com>
2025-08-09 14:36:53 +0000

    Added sitemap

--- client/src/Credits.js
+++ client/src/Credits.js
@@ -56,6 +56,7 @@ function CreditsPage() {
       licenseUrl: "https://www.svgrepo.com/page/licensing/",
       changes: "Modified from original"
     },
+
     {
       title: "Chat Icon",
       author: "Freepik",
@@ -65,6 +66,7 @@ function CreditsPage() {
       licenseUrl: "https://www.freepikcompany.com/legal",
       changes: "No changes made"
     }
+    
   ];
 
   return (
